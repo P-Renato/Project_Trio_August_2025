09e628026ed0334db8dae2a5d33048260eae3676
Renato <paulo.borupdebakker@dci-student.org>
2025-08-19 17:52:17 +0200

    fetching done

--- src/app/api/products/route.tsx
+++ src/app/api/products/route.tsx
@@ -1,12 +1,9 @@
 import { NextResponse } from "next/server";
-import { Product } from "@/types/products";
 
+const API_URL = "https://all-apis.com/products";
 
 // GET /api/products
-export async function GET(request: Request, {params}: {params: {pid: string}}) {
-    const { pid } = params;
-    const API_URL = `https://all-apis.com/products/${pid}`;
-
+export async function GET() {
   try {
     const res = await fetch(API_URL, {
       cache: "no-store", 
@@ -16,9 +13,9 @@ export async function GET(request: Request, {params}: {params: {pid: string}}) {
       throw new Error(`Failed to fetch products: ${res.statusText}`);
     }
 
-    const product = await res.json();
+    const products = await res.json();
 
-    return NextResponse.json({product});
+    return NextResponse.json(products);
   } catch (error: any) {
     return NextResponse.json(
       { error: error.message || "Failed to fetch products" },
---
+++ src/lib/products.ts
@@ -0,0 +1,19 @@
+import { Product } from "@/types/products";
+
+
+// Here is the fectching from all the products
+export async function fetchProducts(): Promise<Product[]> {
+    const res = await fetch('/api/products', { cache: "no-store"});
+    if (!res.ok) throw new Error("Failed to fetch products");
+    const data = await res.json();
+    return data.products;
+}
+
+
+// Here is the fetching from a single product
+export async function fetchProductById(id: string | number): Promise<Product> {
+    const res = await fetch(`/api/products/${id}`, {cache: "no-store"});
+    if(!res.ok) throw new Error('Failed to fetch product');
+    const data = await res.json();
+    return data.product;
+}
