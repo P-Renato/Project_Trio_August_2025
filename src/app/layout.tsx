dad458516a46d6a506299e12dcea4f61fe402759
Muditha <muditha.jayasooriya@dci-student.org>
2025-08-20 09:12:25 +0200

    muditha changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,11 +13,11 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
-        <title>OnlineShop</title>
+        <title>sprint onlineshop</title>
       </head>
       <body className="flex flex-col h-screen">
         <header className="py-4 px-6 md:px-12 bg-gray-50 text-gray-600 flex justify-between">
-          <span className="font-bold">OnlineShop</span>
+          <span className="font-bold"> Sprint online shop</span>
           <nav>
             <ul className="flex gap-4">
               <li className="hover:text-orange-600 cursor-pointer">
