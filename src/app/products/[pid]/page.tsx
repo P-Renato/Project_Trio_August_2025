b1c419cd237cb12bf1570a0a3a3baafc86d201ae
Renato <paulo.borupdebakker@dci-student.org>
2025-08-21 10:38:41 +0200

    some styles

--- src/app/products/[pid]/page.tsx
+++ src/app/products/[pid]/page.tsx
@@ -8,6 +8,7 @@ import { fetchProductById } from "@/lib/products";
 import { useCart } from "@/context/CartContext";
 import Image from "next/image";
 import { Geist } from 'next/font/google';
+import productStyle from './product.module.css'
  
 const geist = Geist({
   subsets: ['latin'],
@@ -24,7 +25,7 @@ function ProductDetails() {
     if (!pid) return;
     fetchProductById(pid as string)
       .then(setProduct)
-      .catch((err) => console.error(err));
+      .catch((err)=> console.error(err))
   }, [pid]);
 
   console.log(product)
@@ -33,18 +34,24 @@ function ProductDetails() {
     <div>
         
       {product ? (
-        <div>
-          <h2 className={geist.className}>{product.name}</h2>
-          <p>{product.category}</p>
-          <p>&euro;{product.price.toFixed(2)}</p>
-          <ul className="flex flex-wrap">
-            {product.image.map((img, i) => (
-              <li key={i} className="w-xs">
-                <Image src={img} alt={`${product.name} ${i}` } width={400} height={200}/>
-              </li>
-            ))}
-          </ul>
-          <button onClick={()=>dispatch({type: 'ADD_TO_CART', payload:product})}>Add to Cart</button>
+        <div className="flex flex-row">
+          
+          <nav className={`${productStyle.image_container}`}>
+            <ul className="flex flex-wrap *:m-2 ">
+              {product.image.map((img, i) => (
+                <li key={i} className="w-xs">
+                  <Image src={img} alt={`${product.name} ${i}` } width={400} height={200}/>
+                </li>
+              ))}
+            </ul>
+          </nav>
+          <nav className="m-6 p-6 *:m-3">
+            <h2 className={geist.className}>{product.name}</h2>
+            <p>{product.category}</p>
+            <p>&euro;{product.price.toFixed(2)}</p>
+            <button className={`${productStyle.add_to_cart_button}`} onClick={()=>dispatch({type: 'ADD_TO_CART', payload:product})}>Add to Cart</button>
+          </nav>
+
         </div>
       ) : (
         <p>Loading product...</p>
